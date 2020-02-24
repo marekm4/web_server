fd6c275c558fa3c8b924c1814e31a749f9eb6184
Marek Michalik <marekmichalik43@gmail.com>
2020-02-24 21:55:56 +0100

    Small refactor

--- src/main.rs
+++ src/main.rs
@@ -21,14 +21,14 @@ fn handle_connection(mut stream: TcpStream) {
     let get = b"GET / HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+        ("200 OK", "hello.html")
     } else {
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+        ("404 NOT FOUND", "404.html")
     };
 
     let contents = fs::read_to_string(filename).unwrap();
 
-    let response = format!("{}{}", status_line, contents);
+    let response = format!("HTTP/1.1 {}\r\n\r\n{}", status_line, contents);
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
